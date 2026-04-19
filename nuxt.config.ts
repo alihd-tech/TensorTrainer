17359f78c491a84b2d53899818dd2191ae2d0fa3
Ali HD <85040609+alihd-tech@users.noreply.github.com>
2026-04-19 07:23:52 +0330

    Add files via upload

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,6 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: false },
   css: ['~/assets/css/main.css'],
-  modules: ["@nuxthub/core"], // Remove "nitro-cloudflare-dev"
 
   postcss: {
     plugins: {
@@ -12,11 +11,6 @@ export default defineNuxtConfig({
   },
 
   nitro: {
-    preset: "cloudflare-pages", // Try this instead of cloudflare_module
-    
-    cloudflare: {
-      deployConfig: true,
-      nodeCompat: true
-    }
+    preset: "cloudflare-pages"
   }
 })
